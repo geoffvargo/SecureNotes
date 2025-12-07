2c6817edd422f551a589223aa45e30f878c0fb90
Geoff Vargo <geoffvargo@gmail.com>
2025-12-06 22:39:11 -0500

    INITIAL COMMIT (lesson 3.32)

---
+++ src/main/java/com/geoffvargo/securenotes/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.geoffvargo.securenotes.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+}
---
+++ src/main/java/com/geoffvargo/securenotes/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.geoffvargo.securenotes.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.*;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
+}
