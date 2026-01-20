574549dc4286df2dc8cec9871ff7dac889538196
Geoff Vargo <geoffvargo@gmail.com>
2026-01-19 19:34:02 -0500

    Auditing — Recording Audit Events

--- src/main/java/com/geoffvargo/securenotes/services/impl/NoteServiceImpl.java
+++ src/main/java/com/geoffvargo/securenotes/services/impl/NoteServiceImpl.java
@@ -14,26 +14,43 @@ public class NoteServiceImpl implements NoteService {
 	@Autowired
 	private NoteRepository noteRepository;
 	
+	private AuditLogService auditLogService;
+	
 	@Override
 	public Note createNoteForUser(String username, String content) {
 		Note note = new Note();
+		
 		note.setOwnerUsername(username);
 		note.setContent(content);
-		return noteRepository.save(note);
+		
+		Note savedNote = noteRepository.save(note);
+		
+		auditLogService.logNoteCreation(username, note);
+		
+		return savedNote;
 	}
 	
 	@Override
 	public Note updateNoteForUser(Long noteId, String content, String username) {
 		Note note = noteRepository.findById(noteId)
-			            .orElseThrow(
-							() -> new RuntimeException("Note not found"));
+		                          .orElseThrow(
+			                          () -> new RuntimeException("Note not found"));
 		note.setContent(content);
-		return noteRepository.save(note);
+		Note saved = noteRepository.save(note);
+		auditLogService.logNoteUpdate(username, note);
+		
+		return saved;
 	}
 	
 	@Override
 	public void deleteNoteForUser(Long noteId, String username) {
-		noteRepository.deleteById(noteId);
+		Note note = noteRepository.findById(noteId).orElseThrow(
+			() -> new RuntimeException("Note not found.")
+		);
+		
+		auditLogService.logNoteDeletion(username, noteId);
+		
+		noteRepository.delete(note);
 	}
 	
 	@Override
